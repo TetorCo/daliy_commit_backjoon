3571d3adf0beeb85a64053f7534bb041f1457261
김태범 <76984534+TetorCo@users.noreply.github.com>
2021-12-18 18:55:53 +0900

    21/12/18

---
+++ 211218_2751.py
@@ -0,0 +1,23 @@
+"""
+수 정렬하기 2
+
+문제
+N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수가 주어진다. 이 수는 절댓값이 1,000,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
+
+출력
+첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+"""
+import sys
+
+n = int(input())
+sort_list = []
+
+for _ in range(n):
+    num = int(sys.stdin.readline())
+    sort_list.append(num)
+
+for num in sorted(sort_list):
+    print(num)
